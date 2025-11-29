671d57761cdea1570c06f5ce046cbb6d69a3cfb0
CAR-GAME <tamimiqbal896@gmail.com>
2025-11-29 17:18:51 +0600

    Header

--- header.php
+++ header.php
@@ -18,14 +18,14 @@
         <div class="row align-items-center">
             
             <!-- LOGO -->
-            <div class="col-xl-3 col-md-2 col-sm-2 header_colum_one">
+            <div class="col-xl-3 col-md-2 col-sm-2 header_column_one">
                 <div class="header-logo">
                     <img src="<?php echo esc_url( get_theme_mod('header_logo') ); ?>" alt="Logo">
                 </div>
             </div>
 
             <!-- MENU -->
-            <div class="col-xl-7 col-md-8 col-sm-8 header_colum_two">
+            <div class="col-xl-7 col-md-8 col-sm-8 header_column_two">
                 <nav class="header-menu">
                     <?php 
                     wp_nav_menu(array(
@@ -37,7 +37,7 @@
             </div>
 
             <!-- EXTRA BUTTON -->
-            <div class="col-xl-2 col-md-2 col-sm-2 text-end header_colum_three">
+            <div class="col-xl-2 col-md-2 col-sm-2 text-end header_column_three">
                 <div class="header-extra">
                     <?php $cv_pdf_url = get_theme_mod('cv_pdf_file'); ?>
                     <?php if($cv_pdf_url): ?>
