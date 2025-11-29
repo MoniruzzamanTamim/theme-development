8e245d883ab90574021461abff30200347b11883
CAR-GAME <tamimiqbal896@gmail.com>
2025-11-29 17:00:45 +0600

    default.php

--- functions/default.php
+++ functions/default.php
@@ -1,4 +1,4 @@
-<?php
-add_theme_support('title-tag');
 
-// Website Title Dynamically Show 
+<?php
+// This Templete is Showing Theme Default Configuration..........
+add_theme_support('title-tag');  // Website Title Dynamically Show 
